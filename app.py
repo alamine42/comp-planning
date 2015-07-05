946dd71804a6e4362131df284b6525416514dd05
alamine42 <alamine@gmail.com>
2015-07-05 15:15:44 -0400

    Added code to pull the last 7 posts from the tumblr blog

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from pytumblr import TumblrRestClient
 
 # Create application object
 app = Flask(__name__)
+
+# Error Handling
+app.config["DEBUG"] = True
 
 # Using decorators, link the application to a url
 @app.route("/")
@@ -9,7 +13,26 @@ app = Flask(__name__)
 
 # Define the view using a function, which returns a string
 def view_week():
-    return "The week's WOD go here."
+
+    client = TumblrRestClient(
+        'q3K5ba44O1DLAv39TDsp4fEMNY1pGAdXsiciIQQqZdAsXF54Zt',
+        'UnkAeG6I7PRHidVqikB3zcrL6DI94q0woJv5sIeUCj3B7ndCdJ',
+        'KT75Gar86W3elQAuU7VyiT9UUstfX6JAFGTvG01pJYKStqlAXN',
+        'ZggEOt83VSC2lzvN01SWmFJy98r13oeuWyciXxfxVqOwAvo81n'
+    )
+
+    print client.info()
+
+    last_seven_posts_json = client.posts('cfsacompetition.tumblr.com', type='text', limit=7)
+
+    return str(last_seven_posts_json)
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "mehdi":
+        return "Hello, {}".format(name), 200
+    else:
+        return "Not Found", 404
 
 # Start the development server using the run() method:
 if __name__ == "__main__":
